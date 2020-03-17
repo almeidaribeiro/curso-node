cb41640f45f4692d43fc2756016d04e3c2589698
Bruna Almeida <almeidaribeiro.b@gmail.com>
2020-03-17 19:25:03 -0300

    first step

---
+++ noticias.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const server = http.createServer(function(req, res){
+    const categoria = req.url 
+
+    if(categoria == '/tecnologia'){
+        res.end('<html><body>Noticias de Tecnologia</body></html>')
+    } else if(categoria == '/moda'){
+        res.end('<html><body>Noticias de moda</body></html>')
+    } else if(categoria == '/beleza'){
+        res.end('<html><body>Noticias de beleza</body></html>')
+    } else {
+        res.end('<html><body>Portal de noticias</body></html>')
+    }
+}).listen(3000)
